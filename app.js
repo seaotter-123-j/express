7077405a55ac17051db98d0bae7db76954474d31
testA <78136137+s-baba-12345@users.noreply.github.com>
2025-07-17 12:32:34 +0900

    add hello page 2

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var helloRouter = require('morgan');
+var helloRouter = require('./routes/hello');
 
 var app = express();
 
